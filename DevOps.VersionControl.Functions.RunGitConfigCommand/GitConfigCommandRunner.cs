1930547ea49580591458e25c170b4a8f17018c4f
Christopher Dorst <chris@dorst.email>
2017-12-27 19:37:42 -0600

    Adding GitConfigCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitConfigCommand/GitConfigCommandRunner.cs
@@ -0,0 +1,15 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitConfigCommand
+{
+    public static class GitConfigCommandRunner
+    {
+        private const string config = nameof(config);
+
+        public static void Config(string directory, string key, string value)
+            => Git(directory,
+                command: config,
+                key,
+                value);
+    }
+}
